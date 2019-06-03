9456cb5f0730c08d320a824e37db4bf2cf7d23b3
darboy <974782852@qq.com>
2019-06-03 22:17:04 +0800

    add driver and data

--- FT4232H-Test/main.cpp
+++ FT4232H-Test/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <Windows.h>
 #include "FT4232H_DRIVER/ftd2xx.h"
-
+//#pragma comment(lib,"ftd2xx.lib")
 using namespace std;
 
 int main()
@@ -12,17 +12,32 @@ int main()
 	ftStatus = FT_Open(0, &ftHandle);
 	if (ftStatus != FT_OK) {
 		// FT_Open failed
+		printf("\r\nFT_OPEN FAILED\r\n");
 		//return ;
 	}
-	UCHAR Mask = 1; //	bit set '0' used for input, set '1' used for output
+	UCHAR Mask = 0x02; //	bit set '0' used for input, set '1' used for output
 	UCHAR Mode = 1; // set Asynchronous Bit Bang mode	//this mode can be used for GPIO
 	ftStatus = FT_SetBitMode(ftHandle, Mask, Mode);
 	if (ftStatus == FT_OK) {
 		// Asynchronous Bit bang Mode enabled
+		printf("bitbang is ok\r\n");
 	}
 	else {
 		// FT_SetBitMode FAILED!
 	}
+	//while (1) {
+	//	UCHAR BitMode;
+	//	ftStatus = FT_GetBitMode(ftHandle, &BitMode);
+	//	if (ftStatus == FT_OK) {
+	//		// BitMode contains current value
+	//	}
+	//	else {
+	//		// FT_GetBitMode FAILED!
+	//		printf("FT_GetBitMode is failed\r\n");
+	//	}
+	//	printf("bit mode is 0x%02x\r\n", BitMode);
+	//	Sleep(100);
+	//}
 	UCHAR BitMode;
 	ftStatus = FT_GetBitMode(ftHandle, &BitMode);
 	if (ftStatus == FT_OK) {
